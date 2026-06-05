45dd809d850c9bdd01964e5feb7590a0ae100839
German World Club <germanworldclub@Mac-mini.local>
2026-06-05 09:54:20 +0400

    Lets test the output first

--- services/whatsAppSender.js
+++ services/whatsAppSender.js
@@ -386,7 +386,10 @@ async function sendMessageToPhone(
         throw new Error(`Unsupported template type: ${templateType}`);
     }
 
-    const result = await twilioClient.messages.create(messageOptions);
+    console.log(messageOptions);
+    console.log(contactPayload);
+
+    // const result = await twilioClient.messages.create(messageOptions);
 
     await Promise.resolve(
       db
